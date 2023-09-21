624e66d26324dee6c18a57d2d274565f224f05ce
Nurislam <nurislamnr1186@gmail.com>
2023-09-21 19:19:20 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import Peer from 'peerjs';
 
 function App() {
+  const myVideoRef = useRef();
+  const remoteVideoRef = useRef();
+  const peer = new Peer(); // Peer sunucusu bağlantısı (kendi sunucunuzu yapılandırmalısınız)
+
+  useEffect(() => {
+    const initializeVideoChat = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+
+        myVideoRef.current.srcObject = stream;
+
+        peer.on('open', (id) => {
+          // Diğer kullanıcıları dinle
+          peer.on('call', (call) => {
+            call.answer(stream); // Gelen çağrıyı yanıtla
+            call.on('stream', (remoteStream) => {
+              // Diğer kullanıcının video akışını görüntüle
+              remoteVideoRef.current.srcObject = remoteStream;
+            });
+          });
+        });
+
+      } catch (error) {
+        console.error('Hata:', error);
+      }
+    };
+
+    initializeVideoChat();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Video Sohbet Uygulaması</h1>
+      <div className="video-container">
+        <video ref={myVideoRef} autoPlay muted style={{ width: 400 }}></video>
+        <video ref={remoteVideoRef} autoPlay style={{ width: 400 }}></video>
+      </div>
     </div>
   );
 }
